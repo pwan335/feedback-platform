8b60151f03e7c168af6014d0aadfc45225f65b9e
Manson-chen <948936249@qq.com>
2022-09-19 23:37:36 +0800

    create server framwork

---
+++ server/easyfeedback/src/main/java/com/elec5619/domain/User.java
@@ -0,0 +1,5 @@
+package com.elec5619.domain;
+
+public class User {
+
+}
---
+++ server/easyfeedback/src/test/java/com/elec5619/EasyfeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elec5619;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyfeedbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
